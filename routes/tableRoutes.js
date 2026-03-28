ec2024944fccaa01135c4f6f47f53d192f76abf4
Siva2232 <sivadathanmb@gmail.com>
2026-03-28 20:57:38 +0530

    done

--- routes/tableRoutes.js
+++ routes/tableRoutes.js
@@ -6,11 +6,11 @@ const {
   removeTable,
 } = require("../controllers/tableController");
 // Assuming there is a protect/admin middleware in authMiddleware.js
-const { protect, admin } = require("../middleware/authMiddleware");
+const { protect, admin, adminOrWaiter } = require("../middleware/authMiddleware");
 
 // In this simple setup, everyone has access to GET (needed for customer choosing mode), 
 // but others are protected/admin
-router.route("/").get(getTables).post(protect, admin, addTable);
+router.route("/").get(getTables).post(protect, adminOrWaiter, addTable);
 router.route("/:id").delete(protect, admin, removeTable);
 
 module.exports = router;
